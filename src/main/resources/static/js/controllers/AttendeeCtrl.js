492a47afec675c27379d0d0497dbbd395746da95
Aarambh Mishra <mishraaarambh@gmail.com>
2016-06-11 14:43:02 +0530

    UI refactoring
    set up for the angular routing and including templates for attendee
    side view

--- src/main/resources/static/js/controllers/AttendeeCtrl.js
+++ src/main/resources/static/js/controllers/AttendeeCtrl.js
@@ -1,3 +1,22 @@
 function AttendeeCtrl($scope){
+	$scope.formSteps = [
+						{"stepNumber" : 0, "title" : "Questions", "view" : "question"}, 
+						{"stepNumber" : 1, "title" : "Calendar", "view" : "calendar"}, 
+						{"stepNumber" : 2, "title" : "Details", "view" : "detail"},
+						{"stepNumber" : 3, "title" : "Confirmation", "view" : "confirmation"}
+						];
+	
+	$scope.currentStep = $scope.formSteps[0];
+	
+	$scope.nextStep = function(){
+		$scope.currentStep.stepNumber++;
+		$scope.currentStep = $scope.formSteps[$scope.currentStep.stepNumber];
+		
+	}
+	
+	$scope.previousStep = function(){
+		$scope.currentStep.stepNumber--;
+		$scope.currentStep = $scope.formSteps[$scope.currentStep.stepNumber];
+	}
 	
 }
--- src/main/resources/static/js/module.js
+++ src/main/resources/static/js/module.js
@@ -10,14 +10,13 @@ app.services = angular.module('services', ['ngResource']);
 app.module = angular.module(app.name, ['ngRoute', 'services', 'ngAnimate', 'toaster', 'mwl.calendar', 'ui.bootstrap']);
 
 app.module.run(function($rootScope, $location){
-	alert('in run function')
 	$rootScope.$location = $location;
 });
 
 app.module.config(['$httpProvider', '$routeProvider', function($httpProvider, $routeProvider) {
     $routeProvider
-        .when('/:eventId/attendee',             { controller: AttendeeCtrl, templateUrl: '/views/attendee.html' })
-        .otherwise({redirectTo: '/1234/attendee' });
+        .when('/event/:eventId/attendee',             { controller: AttendeeCtrl, templateUrl: '/views/attendee.html' })
+        .otherwise({redirectTo: '/' });
     
     $httpProvider.interceptors.push('httpInterceptor');
     
